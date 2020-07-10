c0d6bdd211d881aa94c3500f5466c2228cdd332c
evan <0x4861@beeglelabs.com>
2020-07-11 00:31:40 +0900

    [Add] Login Ui

--- insta_clone_flutter_firebase/lib/root_page.dart
+++ insta_clone_flutter_firebase/lib/root_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:instaflutter/login_page.dart';
 import 'package:instaflutter/tab_page.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }
