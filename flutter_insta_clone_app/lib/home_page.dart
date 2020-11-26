90716ed96377445dd4f58ef5d62d93a86f33fb2a
beegle-evan <0x4861@beeglelabs.com>
2020-11-27 02:50:31 +0900

    [Add] bottom nav

--- flutter_insta_clone_app/lib/home_page.dart
+++ flutter_insta_clone_app/lib/home_page.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
-class home_page extends StatelessWidget {
-  const home_page({
+class HomePage extends StatelessWidget {
+  HomePage({
     Key key,
   }) : super(key: key);
+
+  List<BottomNavigationBarItem> btmNavItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.healing), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: ''),
+  ];
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('cool app'),
-          ),
           body: Container(
             color: Colors.amberAccent,
           ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: btmNavItems,
+            unselectedItemColor: Colors.grey,
+            selectedItemColor: Colors.black87,
+            currentIndex: _selectedIndex,
+            onTap: _onBtmItemClick,
+          ),
         ));
+  }
+
+  void _onBtmItemClick(int index) {
+    print(index);
   }
 }
