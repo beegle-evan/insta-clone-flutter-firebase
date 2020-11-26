6ff4e71725a917b3c14ed27dc4ece38c6aa78250
beegle-evan <0x4861@beeglelabs.com>
2020-11-27 02:38:42 +0900

    [add]  new project for study

---
+++ flutter_insta_clone_app/lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class home_page extends StatelessWidget {
+  const home_page({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('cool app'),
+          ),
+          body: Container(
+            color: Colors.amberAccent,
+          ),
+        ));
+  }
+}
---
+++ flutter_insta_clone_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_insta_clone_app/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return home_page();
+  }
+}
