ed6f82ddccbcb64838193815e5e10f9a713eec9a
beegle-evan <0x4861@beeglelabs.com>
2020-12-03 18:30:05 +0900

    [Add] Sign up screen

--- flutter_insta_clone_app/lib/screens/auth_screen.dart
+++ flutter_insta_clone_app/lib/screens/auth_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_insta_clone_app/widgets/fadestack.dart';
-import 'package:flutter_insta_clone_app/widgets/sign_in_form.dart';
-import 'package:flutter_insta_clone_app/widgets/sign_up_form.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreenState createState() => _AuthScreenState();
 }
 
+const int selectedFormTypeSignUp = 0;
+const int selectedFormTypeSignIn = 1;
+
 class _AuthScreenState extends State<AuthScreen> {
-  int selectedForm = 0;
+  int selectedForm = selectedFormTypeSignUp;
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +21,44 @@ class _AuthScreenState extends State<AuthScreen> {
             FadeStack(
               selectedForm: selectedForm,
             ),
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    if (selectedForm == 0) {
-                      selectedForm = 1;
-                    } else {
-                      selectedForm = 0;
-                    }
-                  });
-                },
-                child: Text('go to sign up'),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              height: 40,
+              child: Container(
+                color: Colors.white,
+                child: FlatButton(
+                  shape: Border(
+                    top: BorderSide(color: Colors.grey),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (selectedForm == selectedFormTypeSignUp) {
+                        selectedForm = selectedFormTypeSignIn;
+                      } else {
+                        selectedForm = selectedFormTypeSignUp;
+                      }
+                    });
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      text: (selectedForm == selectedFormTypeSignUp)
+                          ? "Already have an account?   "
+                          : "Don't have an account?   ",
+                      style: TextStyle(color: Colors.grey),
+                      children: [
+                        TextSpan(
+                          text: (selectedForm == selectedFormTypeSignUp) ? "Sign In " : "Sign Up",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
--- flutter_insta_clone_app/lib/widgets/sign_up_form.dart
+++ flutter_insta_clone_app/lib/widgets/sign_up_form.dart
@@ -1,10 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_insta_clone_app/constants/common_size.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _pwController = TextEditingController();
+  TextEditingController _cpwController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _pwController.dispose();
+    _cpwController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Padding(
+      padding: const EdgeInsets.all(common_gap),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: common_l_gap,
+            ),
+            Image.asset('assets/images/insta_text_logo.png'),
+            TextFormField(
+              controller: _emailController,
+              cursorColor: Colors.black54,
+              decoration: _textInputDecor('Email'),
+              validator: (text) {
+                if (text.isNotEmpty && text.contains("@")) {
+                  return null;
+                } else {
+                  return '정확한 이메일 주소를 입력해주세요.';
+                }
+              },
+            ),
+            SizedBox(
+              height: common_xs_gap,
+            ),
+            TextFormField(
+              controller: _pwController,
+              cursorColor: Colors.black54,
+              obscureText: true,
+              decoration: _textInputDecor('Password'),
+              validator: (text) {
+                if (text.isNotEmpty && text.length > 5) {
+                  return null;
+                } else {
+                  return '비밀번호를 다시 확인해주세요.';
+                }
+              },
+            ),
+            SizedBox(
+              height: common_xs_gap,
+            ),
+            TextFormField(
+              controller: _cpwController,
+              cursorColor: Colors.black54,
+              obscureText: true,
+              decoration: _textInputDecor('Confirm Password'),
+              validator: (text) {
+                if (text.isNotEmpty && _pwController.text == text) {
+                  return null;
+                } else {
+                  return '입력한 값이 비밀번호와 일치하지 않습니다.';
+                }
+              },
+            ),
+            FlatButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  // TextFormField에서, validator의 체크 후, return null이 오는 경우, 이 조건문은 true가 된다.
+                  print('Validation success!!');
+                }
+              },
+              child: Text(
+                'Join',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(common_s_gap),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration _textInputDecor(String hint) {
+    return InputDecoration(
+      hintText: hint,
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.grey[300],
+        ),
+        borderRadius: BorderRadius.circular(common_s_gap),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.redAccent,
+        ),
+        borderRadius: BorderRadius.circular(common_s_gap),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.grey[300],
+        ),
+        borderRadius: BorderRadius.circular(common_s_gap),
+      ),
+      filled: true, // textformfield 안에 배경 색상을 넣기 위해서는, 이 값을 true로 하고 color를 채워줘야함
+      fillColor: Colors.grey[100],
     );
   }
 }
