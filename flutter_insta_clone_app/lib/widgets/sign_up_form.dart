e3c2e19b96c73fa83f0f7cdf9bad166956e8396c
beegle-evan <0x4861@beeglelabs.com>
2020-12-03 18:40:46 +0900

    [Mod] keyboard에 따른 화면 조절

--- flutter_insta_clone_app/lib/widgets/sign_up_form.dart
+++ flutter_insta_clone_app/lib/widgets/sign_up_form.dart
@@ -23,77 +23,80 @@ class _SignUpFormState extends State<SignUpForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(common_gap),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: common_l_gap,
-            ),
-            Image.asset('assets/images/insta_text_logo.png'),
-            TextFormField(
-              controller: _emailController,
-              cursorColor: Colors.black54,
-              decoration: _textInputDecor('Email'),
-              validator: (text) {
-                if (text.isNotEmpty && text.contains("@")) {
-                  return null;
-                } else {
-                  return '정확한 이메일 주소를 입력해주세요.';
-                }
-              },
-            ),
-            SizedBox(
-              height: common_xs_gap,
-            ),
-            TextFormField(
-              controller: _pwController,
-              cursorColor: Colors.black54,
-              obscureText: true,
-              decoration: _textInputDecor('Password'),
-              validator: (text) {
-                if (text.isNotEmpty && text.length > 5) {
-                  return null;
-                } else {
-                  return '비밀번호를 다시 확인해주세요.';
-                }
-              },
-            ),
-            SizedBox(
-              height: common_xs_gap,
-            ),
-            TextFormField(
-              controller: _cpwController,
-              cursorColor: Colors.black54,
-              obscureText: true,
-              decoration: _textInputDecor('Confirm Password'),
-              validator: (text) {
-                if (text.isNotEmpty && _pwController.text == text) {
-                  return null;
-                } else {
-                  return '입력한 값이 비밀번호와 일치하지 않습니다.';
-                }
-              },
-            ),
-            FlatButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  // TextFormField에서, validator의 체크 후, return null이 오는 경우, 이 조건문은 true가 된다.
-                  print('Validation success!!');
-                }
-              },
-              child: Text(
-                'Join',
-                style: TextStyle(color: Colors.white),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Padding(
+        padding: const EdgeInsets.all(common_gap),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: common_l_gap,
               ),
-              color: Colors.blue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(common_s_gap),
+              Image.asset('assets/images/insta_text_logo.png'),
+              TextFormField(
+                controller: _emailController,
+                cursorColor: Colors.black54,
+                decoration: _textInputDecor('Email'),
+                validator: (text) {
+                  if (text.isNotEmpty && text.contains("@")) {
+                    return null;
+                  } else {
+                    return '정확한 이메일 주소를 입력해주세요.';
+                  }
+                },
               ),
-            ),
-          ],
+              SizedBox(
+                height: common_xs_gap,
+              ),
+              TextFormField(
+                controller: _pwController,
+                cursorColor: Colors.black54,
+                obscureText: true,
+                decoration: _textInputDecor('Password'),
+                validator: (text) {
+                  if (text.isNotEmpty && text.length > 5) {
+                    return null;
+                  } else {
+                    return '비밀번호를 다시 확인해주세요.';
+                  }
+                },
+              ),
+              SizedBox(
+                height: common_xs_gap,
+              ),
+              TextFormField(
+                controller: _cpwController,
+                cursorColor: Colors.black54,
+                obscureText: true,
+                decoration: _textInputDecor('Confirm Password'),
+                validator: (text) {
+                  if (text.isNotEmpty && _pwController.text == text) {
+                    return null;
+                  } else {
+                    return '입력한 값이 비밀번호와 일치하지 않습니다.';
+                  }
+                },
+              ),
+              FlatButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    // TextFormField에서, validator의 체크 후, return null이 오는 경우, 이 조건문은 true가 된다.
+                    print('Validation success!!');
+                  }
+                },
+                child: Text(
+                  'Join',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(common_s_gap),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -102,26 +105,30 @@ class _SignUpFormState extends State<SignUpForm> {
   InputDecoration _textInputDecor(String hint) {
     return InputDecoration(
       hintText: hint,
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Colors.grey[300],
-        ),
-        borderRadius: BorderRadius.circular(common_s_gap),
-      ),
-      errorBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Colors.redAccent,
-        ),
-        borderRadius: BorderRadius.circular(common_s_gap),
-      ),
-      focusedErrorBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Colors.grey[300],
-        ),
-        borderRadius: BorderRadius.circular(common_s_gap),
-      ),
+      enabledBorder: _activeInputBorder(),
+      focusedBorder: _activeInputBorder(),
+      errorBorder: _errorInputBorder(),
+      focusedErrorBorder: _errorInputBorder(),
       filled: true, // textformfield 안에 배경 색상을 넣기 위해서는, 이 값을 true로 하고 color를 채워줘야함
       fillColor: Colors.grey[100],
+    );
+  }
+
+  OutlineInputBorder _errorInputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.redAccent,
+      ),
+      borderRadius: BorderRadius.circular(common_s_gap),
+    );
+  }
+
+  OutlineInputBorder _activeInputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey[300],
+      ),
+      borderRadius: BorderRadius.circular(common_s_gap),
     );
   }
 }
