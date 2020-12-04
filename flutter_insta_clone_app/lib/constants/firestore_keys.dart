0c3c0dc79952aeff0f70996f475b1fefb0de5225
beegle-evan <0x4861@beeglelabs.com>
2020-12-05 04:19:43 +0900

    [Add] user model

---
+++ flutter_insta_clone_app/lib/constants/firestore_keys.dart
@@ -0,0 +1,24 @@
+const COLLECTION_POSTS = "Posts";
+const COLLECTION_USERS = "Users";
+
+const KEY_USERKEY = 'user_key';
+const KEY_PROFILEIMG = 'profile_img';
+const KEY_EMAIL = 'email';
+const KEY_LIKEDPOSTS = 'liked_posts';
+const KEY_FOLLOWERS = 'followers';
+const KEY_FOLLOWINGS = 'followings';
+const KEY_MYPOSTS = 'my_posts';
+const KEY_USERNAME = 'username';
+
+const KEY_COMMENT = 'comment';
+const KEY_COMMENTTIME = 'commenttime';
+
+const KEY_POSTIMG = 'postimg';
+const KEY_POSTURI = 'posturi';
+const KEY_CAPTION = 'caption';
+const KEY_LASTCOMMNET = 'last_comment';
+const KEY_LASTCOMMENTOR = 'last_commentor';
+const KEY_LASTCOMMENTTIME = 'last_commenttime';
+const KEY_NUMOFLIKES = 'num_of_likes';
+const KEY_NUMOFCOMMENTS = 'num_of_comments';
+const KEY_POSTTIME = 'post_time';
