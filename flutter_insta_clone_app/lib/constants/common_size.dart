5138540befa67c6cb5c963857a65eed8eebf65c9
beegle-evan <0x4861@beeglelabs.com>
2020-12-04 15:29:03 +0900

    [Add] Gallery detail veiw

--- flutter_insta_clone_app/lib/constants/common_size.dart
+++ flutter_insta_clone_app/lib/constants/common_size.dart
@@ -1,7 +1,9 @@
+const double common_xxxxs_gap = 4.0;
+const double common_xxxs_gap = 6.0;
+const double common_xxs_gap = 8.0;
 const double common_xs_gap = 10.0;
 const double common_s_gap = 12.0;
 const double common_gap = 14.0;
 const double common_l_gap = 16.0;
-const double common_xxs_gap = 8.0;
 
 const double avatar_size = 30.0;
--- flutter_insta_clone_app/lib/widgets/my_gallery.dart
+++ flutter_insta_clone_app/lib/widgets/my_gallery.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_insta_clone_app/constants/common_size.dart';
 import 'package:flutter_insta_clone_app/models/gallery_state.dart';
+import 'package:flutter_insta_clone_app/screens/share_post_screen.dart';
 import 'package:local_image_provider/device_image.dart';
+import 'package:local_image_provider/local_image.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 class MyGallery extends StatefulWidget {
@@ -14,18 +22,38 @@ class _MyGalleryState extends State<MyGallery> {
     return Consumer<GalleryState>(builder: (BuildContext context, GalleryState galleryState, Widget child) {
       return GridView.count(
         crossAxisCount: 3,
-        children: getImages(galleryState),
+        children: getImages(context, galleryState),
       );
     });
   }
 
-  List<Widget> getImages(GalleryState galleryState) {
+  List<Widget> getImages(BuildContext context, GalleryState galleryState) {
     return galleryState.images
         .map(
-          (localImage) => Image(
-            image: DeviceImage(localImage),
+          (localImage) => InkWell(
+            onTap: () async {
+              Uint8List bytes = await localImage.getScaledImageBytes(galleryState.localImageProvider, 0.3);
+              final String timerInMilli = DateTime.now().millisecondsSinceEpoch.toString();
+              try {
+                final path = join((await getTemporaryDirectory()).path, '$timerInMilli.png');
+                File imageFile = File(path)..writeAsBytesSync(bytes); // .. 하면 File method를 실행한 결과에 다음 메소드를 실행한다
+
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) => SharePostScreen(imageFile)));
+              } catch (e) {}
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(common_xxxxs_gap),
+              child: Image(
+                image: DeviceImage(localImage, scale: 0.1),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
         )
         .toList();
   }
+
+  // Future<Uint8List> localImageToBytes(GalleryState galleryState, LocalImage localImage) {
+  //   return localImage.getScaledImageBytes(galleryState.localImageProvider, 0.3);
+  // }
 }
