364f2c391db587aa060726f481f50ef48dcb7d8f
beegle-evan <0x4861@beeglelabs.com>
2020-12-06 00:45:37 +0900

    [Add] follow/unfollow

--- flutter_insta_clone_app/lib/repo/user_network_repository.dart
+++ flutter_insta_clone_app/lib/repo/user_network_repository.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_insta_clone_app/constants/firestore_keys.dart';
 import 'package:flutter_insta_clone_app/models/firestore/user_model.dart';
+import 'package:flutter_insta_clone_app/repo/helper/generate_post_key.dart';
 import 'package:flutter_insta_clone_app/repo/helper/transformers.dart';
 
 class UserNetworkRepository with Transformers {
@@ -29,6 +30,40 @@ class UserNetworkRepository with Transformers {
 
   Stream<List<UserModel>> getAllUsersWithoutMe() {
     return Firestore.instance.collection(COLLECTION_USERS).snapshots().transform(toUsersExceptMe);
+  }
+
+  Future<void> followUser({String myUserKey, String otherUserKey}) async {
+    final DocumentReference myUserRef = Firestore.instance.collection(COLLECTION_USERS).document(myUserKey);
+    final DocumentSnapshot mySnapshot = await myUserRef.get();
+    final DocumentReference otherUserRef = Firestore.instance.collection(COLLECTION_USERS).document(otherUserKey);
+    final DocumentSnapshot otherSnapshot = await otherUserRef.get();
+
+    Firestore.instance.runTransaction((tx) async {
+      if (mySnapshot.exists && otherSnapshot.exists) {
+        await tx.update(myUserRef, {
+          KEY_FOLLOWINGS: FieldValue.arrayUnion([otherUserKey])
+        });
+        int currentFollowers = otherSnapshot.data[KEY_FOLLOWERS];
+        await tx.update(otherUserRef, {KEY_FOLLOWERS: currentFollowers + 1});
+      }
+    });
+  }
+
+  Future<void> unfollowUser({String myUserKey, String otherUserKey}) async {
+    final DocumentReference myUserRef = Firestore.instance.collection(COLLECTION_USERS).document(myUserKey);
+    final DocumentSnapshot mySnapshot = await myUserRef.get();
+    final DocumentReference otherUserRef = Firestore.instance.collection(COLLECTION_USERS).document(otherUserKey);
+    final DocumentSnapshot otherSnapshot = await otherUserRef.get();
+
+    Firestore.instance.runTransaction((tx) async {
+      if (mySnapshot.exists && otherSnapshot.exists) {
+        await tx.update(myUserRef, {
+          KEY_FOLLOWINGS: FieldValue.arrayRemove([otherUserKey])
+        });
+        int currentFollowers = otherSnapshot.data[KEY_FOLLOWERS];
+        await tx.update(otherUserRef, {KEY_FOLLOWERS: currentFollowers - 1});
+      }
+    });
   }
 }
 
--- flutter_insta_clone_app/lib/screens/search_screen.dart
+++ flutter_insta_clone_app/lib/screens/search_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_insta_clone_app/models/firestore/user_model.dart';
+import 'package:flutter_insta_clone_app/models/user_model_state.dart';
 import 'package:flutter_insta_clone_app/repo/user_network_repository.dart';
 import 'package:flutter_insta_clone_app/widgets/my_progress_indicator.dart';
 import 'package:flutter_insta_clone_app/widgets/rounded_avatar.dart';
+import 'package:provider/provider.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -12,54 +16,69 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<UserModel>>(
-        stream: userNetworkRepository.getAllUsersWithoutMe(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return SafeArea(
-              child: ListView.separated(
-                  itemBuilder: (context, index) {
-                    UserModel userModel = snapshot.data[index];
-
-                    return ListTile(
-                      onTap: () {
-                        setState(() {
-                          // followings[index] = !followings[index];
-                        });
-                      },
-                      leading: RoundedAvatar(),
-                      title: Text(userModel.username),
-                      subtitle: Text('this is user bio of ${userModel.username}'),
-                      trailing: Container(
-                        height: 30,
-                        width: 80,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.blue[50],
-                          border: Border.all(
-                            color: Colors.blue,
-                            width: 0.5,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Text(
-                          'following',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Follow/Unfollow'),
+      ),
+      body: StreamBuilder<List<UserModel>>(
+          stream: userNetworkRepository.getAllUsersWithoutMe(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return SafeArea(
+                child: Consumer<UserModelState>(
+                  builder: (BuildContext context, UserModelState myUserModelState, Widget child) {
+                    return ListView.separated(
+                        itemBuilder: (context, index) {
+                          UserModel otherUser = snapshot.data[index];
+                          bool amIFollowing = myUserModelState.amIFollowingThisUser(otherUser.userKey);
+                          return ListTile(
+                            onTap: () {
+                              setState(() {
+                                amIFollowing
+                                    ? userNetworkRepository.unfollowUser(
+                                        myUserKey: myUserModelState.userModel.userKey, otherUserKey: otherUser.userKey)
+                                    : userNetworkRepository.followUser(
+                                        myUserKey: myUserModelState.userModel.userKey, otherUserKey: otherUser.userKey);
+                              });
+                            },
+                            leading: RoundedAvatar(),
+                            title: Text(otherUser.username),
+                            subtitle: Text('this is user bio of ${otherUser.username}'),
+                            trailing: Container(
+                              height: 30,
+                              width: 80,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: amIFollowing ? Colors.blue[50] : Colors.red[50],
+                                border: Border.all(
+                                  color: amIFollowing ? Colors.blue : Colors.red,
+                                  width: 0.5,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: FittedBox(
+                                child: Text(
+                                  amIFollowing ? 'following' : 'follow',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return Divider(
+                            color: Colors.grey,
+                          );
+                        },
+                        itemCount: snapshot.data.length);
                   },
-                  separatorBuilder: (context, index) {
-                    return Divider(
-                      color: Colors.grey,
-                    );
-                  },
-                  itemCount: snapshot.data.length),
-            );
-          } else {
-            return MyProgressIndicator();
-          }
-        });
+                ),
+              );
+            } else {
+              return MyProgressIndicator();
+            }
+          }),
+    );
   }
 }
